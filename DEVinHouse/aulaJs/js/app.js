e7bd3e9cc926887074cdde9630b21c7a91474672
Lucas <lucasrukhan@gmail.com>
2022-03-14 22:05:46 -0300

    att js 2.0

---
+++ DEVinHouse/aulaJs/js/app.js
@@ -0,0 +1,8 @@
+var convidado = "Lucas";
+var convidada = "Dayane";
+var noivo = "Thiago";
+var noiva = "Thais";
+
+
+console.log("Caro "  + convidado + " e " + convidada + "!");
+console.log("Vocês estão convidados para o casamento do " + noivo + " e " + noiva + " !");
