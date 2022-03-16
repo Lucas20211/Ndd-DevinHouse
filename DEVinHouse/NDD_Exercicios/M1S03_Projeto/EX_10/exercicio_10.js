4f704c18cacb2df2ea392189dcd247bb1b807b58
Lucas <lucasrukhan@gmail.com>
2022-03-16 19:03:39 -0300

    att exercicio 10 1.2

---
+++ DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_10/exercicio_10.js
@@ -0,0 +1,17 @@
+// criar uma calculadora automatica.
+// laço de repeticao (FOR).
+
+
+function cliqueAqui () {
+
+    var numeroIncial = parseFloat(prompt("digite seu Valor Inicial : " ));
+    var numeroRaiz = parseFloat(prompt("Digite seu Valor Final : "));
+    var termo = 10;       
+    var pa = '';
+    
+    for( var conta = 1; conta <= termo; conta++){
+        pa += conta = (numeroIncial);
+        numeroIncial += numeroRaiz;
+    } 
+    alert("Resultado:  " +pa);
+}
