671a8d29cbe64b67394f9d195ad8b2fba2d50503
Lucas <lucasrukhan@gmail.com>
2022-03-16 10:34:38 -0300

    att exercicio_2 1.1

--- DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_02/exercicio_02.js
+++ DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_02/exercicio_02.js
@@ -1,2 +1,17 @@
 // CRIAR UM BOTÃO DE CONFIRMAR!
 
+//var decisao = confirm("Você desligou AD BLOCK?");
+  //  if(decisao) {
+   //     alert("Agora reinicie o navegador!, \n");
+   // }else {
+    //    alert("Infelizmente seu Download foi cancelado, \n");
+   // }
+    function resposta (){
+        var decisao = confirm("Você desligou AD BLOCK?");
+        if(decisao) {
+            alert("Agora reinicie o navegador! \nE seja feliz!! 😉");
+        }else {
+            alert("Infelizmente sem Download por aqui. 😭 \n");
+        }
+    }
+
