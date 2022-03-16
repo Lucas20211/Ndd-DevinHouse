3ed682ea8abbeaa381f937269cf1980c81b0d206
Lucas <lucasrukhan@gmail.com>
2022-03-16 14:05:16 -0300

    att exercicio 04 1.3

--- DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_04/exercicio_04.js
+++ DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_04/exercicio_04.js
@@ -1,16 +1,34 @@
-const btn = document.querySelector("#send");
-
-btn.addEventListener("click", function(e) {
-
-    e.preventDefault();
-
-    const name = document.querySelector("#name");
-    const idade = document.querySelector("#idade");
-
-    const value = name.value +  idade.value;
-   
+// EXERCICIO 4  FORMULARIO COM TRUE && FALSE
 
 
-    console.log(value);
+//var nome = prompt("Digite seu nome");
 
-});
+function cliqueAqui (){
+    var nome;
+    var idade;
+    var valor;
+    // laco de repetição para declarar os valores
+    do{
+        nome = prompt("Digite seu nome: ");  
+        idade = prompt("Digite sua idade: ");   
+        valor = prompt("Digite numero de 1 a 10 :");         
+    }while
+    (nome == null || nome == " ");
+    (idade == null || idade == "");
+    (valor == null || valor == "");
+    
+    console.log(nome);
+    console.log(idade);
+    
+    // UM "IF" PARA DECLARAR TRUE OR FALSE
+    if((valor >= 1) && ( valor <= 10)){
+        console.log(valor = true);  
+    }else {
+        console.log(valor = false);
+    }
+}
+
+
+
+
+
---
+++ DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_05/exercicio_05.js
@@ -0,0 +1,14 @@
+
+function main(){
+
+    var sobreNome = prompt('Digite seu sobrenome: ');
+    var nome = prompt('digite seu nome: ');
+
+    document.write("Bem vindo, ", nome);
+
+
+}
+
+
+
+main();
