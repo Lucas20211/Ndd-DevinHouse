9626c79d5c0fb31472a9157b26f21d41660a1eb3
Lucas <lucasrukhan@gmail.com>
2022-03-16 19:40:52 -0300

    att aula thais

---
+++ DEVinHouse/exercicios_aulas/aula_thais/aula.js
@@ -0,0 +1,13 @@
+
+
+
+var num1 = 2;
+var num2 = 5;
+
+console.log('Soma: ', num1 + num2);
+console.log('subtração: ', num1 - num2);
+console.log('Multiplicação: ', num1 * num2);
+console.log('Divisão: ', num1 / num2);
+console.log('Potencia: ', num1 ** num2 );
+console.log('Resto: ', num1 & num2 );
+
