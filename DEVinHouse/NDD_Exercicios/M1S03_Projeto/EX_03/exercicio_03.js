87851118db270d8c4103422f5a4d937e52586624
Lucas <lucasrukhan@gmail.com>
2022-03-16 10:49:52 -0300

    att exercicio 03  1.0

---
+++ DEVinHouse/NDD_Exercicios/M1S03_Projeto/EX_03/exercicio_03.js
@@ -0,0 +1,11 @@
+// ALERTA COLOCANDO SEU NOME
+
+function seuNome (){
+    var nome;
+    do {
+        nome = prompt ("Qual seu nome? ");
+    }while
+        (nome == null || nome == "");
+
+    alert("Seu nome é ==> "+ nome +"\nVocê esta aprovado para donwnoad! 😉" );
+}
