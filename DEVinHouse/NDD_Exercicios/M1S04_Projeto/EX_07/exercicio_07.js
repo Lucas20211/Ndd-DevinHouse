18dea279c109b1d86141c0824a7096ac3ca4fa19
Lucas <lucasrukhan@gmail.com>
2022-03-28 13:24:45 -0300

    atualizacao exercicios ( 07, 08, 09, 10 )

---
+++ DEVinHouse/NDD_Exercicios/M1S04_Projeto/EX_07/exercicio_07.js
@@ -0,0 +1,4 @@
+const temperaturaF = [0, 32, 45, 50, 75, 80, 99, 120];
+
+
+const temperaturaC = temperaturaF.map(Math.round (temperaturaF - 32 ) * 5 / 9 );
---
+++ DEVinHouse/NDD_Exercicios/M1S04_Projeto/EX_10/exercicio_10.js
@@ -0,0 +1,14 @@
+contasClientes : [
+    {
+    id: 1,
+    saldo: 500,
+    },
+    {
+    id: 2,
+    saldo: 30000,
+    },
+    {
+    id: 3,
+    saldo: 50,
+    },
+    ];
