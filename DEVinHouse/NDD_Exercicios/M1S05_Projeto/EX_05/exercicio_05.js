cc572ab68deac82e2a2613248237d4bbb3a2c1ab
Lucas <lucasrukhan@gmail.com>
2022-03-31 19:20:25 -0300

    atualizacao exercicio 05

---
+++ DEVinHouse/NDD_Exercicios/M1S05_Projeto/EX_05/exercicio_05.js
@@ -0,0 +1,19 @@
+class Pedidos {
+    numeroPedido;
+    nomeCliente;
+    dataPedido;
+    estaPago;
+    listaPedido;
+
+    constructor(parametro){
+        console.log(parametro);
+    }
+}
+
+new Pedidos("oi Lucas");
+
+//const pedido = new Pedidos();
+
+
+
+//console.log(pedido);
