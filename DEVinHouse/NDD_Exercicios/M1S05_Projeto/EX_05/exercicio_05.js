a37c71252b877dca141d5d80d541679feeb1623b
Lucas <lucasrukhan@gmail.com>
2022-04-01 17:10:29 -0300

    att exercicio 05

--- DEVinHouse/NDD_Exercicios/M1S05_Projeto/EX_05/exercicio_05.js
+++ DEVinHouse/NDD_Exercicios/M1S05_Projeto/EX_05/exercicio_05.js
@@ -1,3 +1,15 @@
+class Produto {
+    constructor( codigoProduto, nomeProduto, precoProduto,quantiProduto){
+        this.codigoProduto = nomeProduto;
+        this.nomeProduto = codigoProduto;
+        this.precoProduto = precoProduto;
+        this.quantiProduto = quantiProduto;
+    }
+}
+
+const mouseProduto = new Produto('001', 'mouse', 50.40, 10.00);
+const tecladoProduto = new Produto( '002', 'teclado', 100.50, 20);
+
 class Pedidos {
     constructor(numeroPedido, nomeCliente, estaPago){
         this.numeroPedido = numeroPedido;
@@ -10,10 +22,13 @@ class Pedidos {
            this.listaPedido.push(produto);
         }   
 }
-    
-const pedido = new Pedidos('01', 'Lucas', false, []);
-    
-    pedido.adicionarProduto('mouse');
-    pedido.adicionarProduto('teclado');
 
-console.log(pedido);
+
+const pedido = new Pedidos('01', 'Lucas', true, []);
+        pedido.adicionarProduto(mouseProduto);
+        pedido.adicionarProduto(tecladoProduto);
+
+        console.log(mouseProduto);
+        console.log(tecladoProduto);
+        console.log(pedido);
+        
