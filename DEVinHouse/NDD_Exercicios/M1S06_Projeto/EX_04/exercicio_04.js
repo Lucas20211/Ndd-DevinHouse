2fce8154a87403cfc51b583d92ae74c1fd25b8a1
Lucas <lucasrukhan@gmail.com>
2022-04-06 11:02:36 -0300

    exercicio 04

---
+++ DEVinHouse/NDD_Exercicios/M1S06_Projeto/EX_04/exercicio_04.js
@@ -0,0 +1,17 @@
+const options = {
+    method: "GET",
+    mode: "cors",
+    caches: "default"
+}
+
+const cep = document.getElementById("cep")
+cep.addEventListener("blur", (e) => {
+    let Cep = document.getElementById("cep").value;
+    console.log(Cep)
+    let search = cep.value.replace("-", "")
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options).then((response) => {
+        response.json().then(data => {
+            console.log(data)
+        })
+    })
+})
