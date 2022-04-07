b01a0020454857a51dda116184d7eff901f07b67
Lucas <lucasrukhan@gmail.com>
2022-04-07 13:47:30 -0300

    att exercicios m1s06 2.1

--- DEVinHouse/NDD_Exercicios/M1S06_Projeto/EX_04/exercicio_04.js
+++ DEVinHouse/NDD_Exercicios/M1S06_Projeto/EX_04/exercicio_04.js
@@ -1,19 +1,35 @@
-
+const limparFormulario = (endereco) =>{
+    document.getElementById('logradouro').value = ' ';
+    document.getElementById('bairro').value = ' ';
+    document.getElementById('localidade').value = ' ';
+    document.getElementById('uf').value = ' ';
+}
 const preencherFormulario = (endereco) =>{
     document.getElementById('logradouro').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('localidade').value = endereco.localidade;
     document.getElementById('uf').value = endereco.uf;
 }
-
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 const pesquisarCep = async() => {
+    limparFormulario();
     const cep = document.getElementById('cep').value;
     const url = `http://viacep.com.br/ws/${cep}/json/`;
-    const dados = await fetch(url);
-    const endereco = await dados.json();
-    preencherFormulario(endereco);
-    
+    if(cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if(endereco.hasOwnProperty('erro')){
+            alert("CEP não encontrado!!");
+            //console.log(endereco);
+        }else{
+            preencherFormulario(endereco);
+        }
+    }else{
+        alert("CEP inválido!!");
+    }    
 }
+
 function enviar(){console.log(document.getElementById('cep').value);}
 
 document.getElementById('cep')
