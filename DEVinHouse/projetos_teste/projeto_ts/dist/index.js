f7e922352ed60335a21912200934b3bc8d827411
Lucas <lucasrukhan@gmail.com>
2022-04-07 21:27:25 -0300

    att aula thais

---
+++ DEVinHouse/projetos_teste/projeto_ts/dist/index.js
@@ -0,0 +1,5 @@
+const calcular = (n1, n2) => {
+    return `resultado ${n1 + n2}`;
+};
+const resultado = calcular(10, 2);
+console.log(resultado);
---
+++ DEVinHouse/projetos_teste/projeto_ts/src/index.ts
@@ -0,0 +1,6 @@
+const calcular = (n1: number, n2: number): string => {
+    return `resultado ${n1 + n2}`;
+};
+
+const resultado = calcular(10,2);
+console.log(resultado);
