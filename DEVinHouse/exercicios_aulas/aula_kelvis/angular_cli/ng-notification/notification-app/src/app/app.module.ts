9b2bd51c1bbdbf851e7a84655750863c1cccd8e4
Lucas <lucasrukhan@gmail.com>
2022-04-22 21:28:09 -0300

    att aula kelvis sexta feira

--- DEVinHouse/exercicios_aulas/aula_kelvis/angular_cli/ng-notification/notification-app/src/app/app.module.ts
+++ DEVinHouse/exercicios_aulas/aula_kelvis/angular_cli/ng-notification/notification-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
