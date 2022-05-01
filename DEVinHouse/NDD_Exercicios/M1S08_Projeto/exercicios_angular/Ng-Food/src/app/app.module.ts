a92e6a1c7a80a438597ed82adb37eb83ac89c291
Lucas <lucasrukhan@gmail.com>
2022-05-01 17:40:10 -0300

    m1s08 angular

--- DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/app.module.ts
+++ DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { MainComponent } from './component/main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
