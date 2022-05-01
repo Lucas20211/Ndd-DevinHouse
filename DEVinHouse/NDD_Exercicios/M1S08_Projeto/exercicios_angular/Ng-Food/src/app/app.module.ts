ff986961030614f2465aa239d6f1486b1db31fff
Lucas <lucasrukhan@gmail.com>
2022-05-01 19:18:53 -0300

    m1s08 angular

--- DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/app.module.ts
+++ DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { MainComponent } from './component/main/main.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ListComponent } from './component/list/list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListasComponent } from './pages/listas/listas.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent,
+    ListComponent,
+    HomeComponent,
+    ListasComponent
   ],
   imports: [
     BrowserModule,
---
+++ DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/pages/listas/listas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngf-listas',
+  templateUrl: './listas.component.html',
+  styleUrls: ['./listas.component.scss']
+})
+export class ListasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
