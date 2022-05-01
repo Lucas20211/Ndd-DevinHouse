85b4bad1c1cfe6f17277897ed17eb359952c27c2
Lucas <lucasrukhan@gmail.com>
2022-05-01 17:26:10 -0300

    exercicio semana 8 angular

--- DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/app.module.ts
+++ DEVinHouse/NDD_Exercicios/M1S08_Projeto/exercicios_angular/Ng-Food/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ConteinerComponent } from './conteiner/conteiner.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    NavbarComponent,
-    ConteinerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
