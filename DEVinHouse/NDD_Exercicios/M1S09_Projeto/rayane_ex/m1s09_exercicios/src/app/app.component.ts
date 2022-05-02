2545fce3d4cd4864fcaa846640143914a868bb7a
Lucas <lucasrukhan@gmail.com>
2022-05-02 12:31:29 -0300

    exercicios semana 09 Rayane

---
+++ DEVinHouse/NDD_Exercicios/M1S09_Projeto/rayane_ex/m1s09_exercicios/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'm1s09_exercicios';
+}
