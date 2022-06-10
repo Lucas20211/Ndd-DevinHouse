aa27692357a22faa1b0ac2a44234bb5a8a200492
Lucas <lucasrukhan@gmail.com>
2022-06-09 21:57:36 -0300

    aula mika 4

--- DEVinHouse/exercicios_aulas/aula_mika/CSharp/aula03/AulaMika/Program.cs
+++ DEVinHouse/exercicios_aulas/aula_mika/CSharp/aula03/AulaMika/Program.cs
@@ -12,7 +12,27 @@ class Program
 
     private static void Run()
     {
-        throw new NotImplementedException();
+       var htmlbuilder = new StringBuilder(string.Empty);
+       htmlbuilder.Append(@"
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Document</title>
+            </head>
+            <body>"
+
+       );
+       do{
+
+       }while(true);
+
+       htmlbuilder.Append(@"
+       </body>
+       </html>
+       ");
     }
 }
     
