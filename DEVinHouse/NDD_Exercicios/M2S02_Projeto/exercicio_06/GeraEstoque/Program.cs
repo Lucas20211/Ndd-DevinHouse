a8dac65b3a6203298bdc9f68985da596fa26f630
Lucas <lucasrukhan@gmail.com>
2022-06-10 16:44:10 -0300

    att semana 2 modulo 2

---
+++ DEVinHouse/NDD_Exercicios/M2S02_Projeto/exercicio_06/GeraEstoque/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GeraEstoque;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string textosaudacao = @"Seja Bem-Vindo ao GeraEstoque 1.0!!";
+
+        System.Console.WriteLine(textosaudacao);
+    }
+}
+
